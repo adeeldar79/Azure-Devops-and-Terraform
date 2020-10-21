16f5bead2da2294ffaab87f779711952a845b836
Adeel Dar <adeeldar79@googlemail.com>
2020-10-21 23:29:24 +0100

    added main.tf

---
+++ main.tf
@@ -0,0 +1,36 @@
+provider "azurerm" {
+    version = "2.5.0"
+    features {}
+}
+
+resource    "azurerm_resource_group" "tf_test" {
+    name = "tfmainrg"
+    location = "UK South"
+}
+
+#init 
+#plan
+#apply
+#destroy
+
+resource    "azurerm_container_group" "tfcg_test" {
+    name                    = "weatherapi"
+    location                = azurerm_resource_group.tf_test.location
+    resource_group_name     = azurerm_resource_group.tf_test.name
+    
+    ip_address_type         = "public"
+    dns_name_label          = "adeeldarwebapi"
+    os_type                 = "Linux"
+
+    container {
+      name            = "weatherapi"
+      image           = "adeeldar/weatherapi"
+        cpu             = "1"
+        memory          = "1"
+
+        ports {
+            port        = 80
+            protocol    = "TCP"
+        }
+    }
+}
